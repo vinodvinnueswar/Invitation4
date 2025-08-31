d716df7237615240265b4fa08db8c98130a13f51
Sasispecious <sasispecious@gmail.com>
2025-08-30 20:47:54 -0700

    Changes in Portrait Screens

---
+++ src/pages/Warning_Page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Mobile from '../Images/Mobile/smartphone.png'
+
+const Warning_Page = () => {
+  return (
+    <div className='landscape-warning'>
+         <img src={Mobile} alt="" />
+        <p>Only in Portrait Mode </p>
+
+    </div>
+
+  )
+}
+
+export default Warning_Page
